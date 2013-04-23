3f1bf0df0b24761245b5accaedfc492042ad0441
em-space <shift.in.emphasis@gmail.com>
2013-04-23 22:57:15 +0200

    Rework the DUCET table into a trie and also store contractions.

--- folio-uca.el
+++ folio-uca.el
@@ -26,13 +26,18 @@
 
 ;;; Code:
 
+(defvar folio-uca-table (make-char-table 'uca-table)
+  "DUCAT table associating code points to collation vectors.
+As of Unicode 6.2.0 this table maintains 24405 single-character
+entries, 723 two-character contractions and 4 tree-character
+contractions.")
 
 (defun folio-uca-make-table-value (levels)
   (let ((weights (make-vector
                   (* (length levels) (+ 2 2 1)) 0))
         (index -1))
     (mapc (lambda (x)
-              ;; 16 bit level 1
+            ;; 16 bit level 1
             (aset weights (setq index (1+ index))
                   (lsh (elt x 0) -8))
             (aset weights (setq index (1+ index))
@@ -49,35 +54,55 @@
                   (logand (elt x 2) #x00ff))) levels)
     weights))
 
+(defsubst folio-uca-char-list-re (char-count)
+  (concat "^\\([0-9a-f]+\\(?:\s+[0-9a-f]+\\)\\{"
+          (format "%d" char-count) "\\}\\)\s+;\s+"))
+
 (defun folio-uca-parse-levels ()
-  (let (collation-levels)
+  (let (levels)
     (while (looking-at "\\[[.*]\\([0-9a-f.]+\\)\\]")
       (let* ((pos (match-end 1))
-             (weights (nbutlast (split-string
-                                 (match-string 1) "[.]" t) 1))
-             (levels (mapcar (lambda (x)
-                               (string-to-number x 16))
-                             weights)))
-        (push levels collation-levels)
+             (match (nbutlast (split-string
+                               (match-string 1) "[.]" t) 1))
+             (level (mapcar (lambda (x)
+                              (string-to-number x 16))
+                            match)))
+        (push level levels)
         (goto-char (1+ pos))))
-    (nreverse collation-levels)))
+    (nreverse levels)))
 
-(defvar folio-uca-table (make-char-table 'uca-table)
-  "")
+(defun folio-uca-table-put (table char-list collation-elements)
+  (let* ((char (pop char-list))
+         (node (aref table char)))
+    (if (null char-list)
+        (setq node (cons (folio-uca-make-table-value
+                          collation-elements) (cdr node)))
+      (setq node (cons (car node)
+                       (or (cdr node)
+                           (make-char-table 'uca-aux-table)))))
+    (aset table char node)
+    (when char-list
+      (folio-uca-table-put
+       (cdr (aref table char)) char-list collation-elements))))
 
 (defun folio-uca-load-table ()
-  (goto-char (point-min))
-  (while (re-search-forward
-          "^\\([0-9a-f]+\\)\s+;\s+" nil t)
-    (let ((char (string-to-number (match-string 1) 16))
-          collation-elements)
+  (let ((char-list-regexp
+         "^\\([0-9a-f]+\\(?:\s+[0-9a-f]+\\)*\\)\s+;\s+"))
+    (goto-char (point-min))
+    (while (re-search-forward char-list-regexp nil t)
+      (let ((chars (mapcar (lambda (x)
+                             (string-to-number x 16))
+                           (split-string
+                            (match-string 1) "[ ]" t)))
+            collation-elements)
       (while (progn
                (push (folio-uca-parse-levels)
                      collation-elements)
                (not (null (car collation-elements)))))
-      (aset folio-uca-table
-            char (folio-uca-make-table-value
-                  (car (nreverse (cdr collation-elements))))))))
+      (setq collation-elements
+            (car (nreverse (cdr collation-elements))))
+      (folio-uca-table-put
+       folio-uca-table chars collation-elements)))))
 
 
 (provide 'folio-uca)
