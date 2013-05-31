0dab366d857db4c751d6704f996e79cac428ee8f
em-space <shift.in.emphasis@gmail.com>
2013-05-31 22:40:57 +0200

    Rename `folio-hash-fnv' from `folio-fnv'.

--- folio-hash.el
+++ folio-hash.el
@@ -58,7 +58,7 @@ Run-time complexity is O\(sqrt\(N))."
             (t 2))
     nil))
 
-(defun folio-fnv (str &optional base)
+(defun folio-hash-fnv (str &optional base)
   "Return a 24-bit hash of the string STR.
 The hash algorithm is 32-bit FNV-1a xor-folded down to 24 bit."
   ;; http://isthe.com/chongo/tech/comp/fnv/
